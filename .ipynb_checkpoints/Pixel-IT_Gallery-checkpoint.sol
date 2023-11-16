09deb726d854b73befc29f58a76722eea2c37b11
Seshouan <session.mwamufiya@gmail.com>
2023-11-16 06:40:33 +0100

    testing out some auction contracts on Remix

--- .ipynb_checkpoints/Pixel-IT_Gallery-checkpoint.sol
+++ .ipynb_checkpoints/Pixel-IT_Gallery-checkpoint.sol
@@ -1,2 +1,28 @@
 pragma solidity ^0.5.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+import "./Simple_Auction.sol"
+
+contract Gallery is ERC721Full {
+    constructor() public ERC721Full("Certificate", "CERT") {}
+
+    function awardCertificate(address student, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newCertificateId = totalSupply();
+        _mint(student, newCertificateId);
+        _setTokenURI(newCertificateId, tokenURI);
+
+        return newCertificateId;
+    }
+}
+
+contract Pixel_IT_deploy {
+    // create tokens for each image
+
+    // create a gallery instance for bidding
+
+    // save the address for the gallery
+
+}
